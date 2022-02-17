c186ef27d45c278ec64a1efc66fbfdb1a81f4201
Nekane Ibarlucea <nknib90@gmail.com>
2022-02-17 13:03:04 +0100

    1

---
+++ rooms-app/routes/room.routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+
+// ℹ️ Handles password encryption
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+
+// How many rounds should bcrypt run the salt (default [10 - 12 rounds])
+const saltRounds = 10;
+
+// Require the User model in order to interact with the database
+const Room = require("../models/Room.model");
+
+// Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
+const isLoggedOut = require("../middleware/isLoggedOut");
+const isLoggedIn = require("../middleware/isLoggedIn");
+
+router.get("/create-room", isLoggedOut, (req, res) => {
+    res.render("rooms/create-room");
+  });
+
+
+
+module.exports = router;
---
+++ rooms-app/utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@cluster0.xkqko.mongodb.net/LAB-ironRooms?retryWrites=true&w=majority`;
+
+module.exports = MONGO_URI
